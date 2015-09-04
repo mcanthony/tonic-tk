00755018672be634669c0a90c0202131f7a4af44
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2015-09-04 15:17:40 -0600

    Add helper for listing the status of tonic components

---
+++ dev-status.sh
@@ -0,0 +1,85 @@
+# Tonic widgets
+cd tonic-widgets
+echo "-------------------------------------------------------------------------"
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ..
+
+# Tonic apps
+cd tonic-apps
+cd arctic-viewer
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ..
+
+cd arctic-viewer-ios
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ../..
+
+# Tonic handlers
+cd tonic-handlers/tonic-mouse-handler
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ../..
+
+# Tonic utils
+cd tonic-utils/tonic-data-manager
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ..
+
+cd tonic-data-converter
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ..
+
+cd tonic-data-generator
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ..
+
+cd tonic-image-builder
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ..
+
+cd tonic-query-data-model
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ..
+
+cd tonic-site-generator
+echo "#" $PWD
+echo
+git fetch origin
+git status | grep -vi master | grep -vi git | grep -vi clean
+echo "-------------------------------------------------------------------------"
+cd ..
